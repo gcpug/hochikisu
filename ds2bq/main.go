4d62406194ff89089268066a82b8d2537239fce0
sinmetal <metal.tie@gmail.com>
2019-12-03 18:05:40 +0900

    jsonがds2bqと噛み合ってなかったのを修正 refs #6

--- ds2bq/main.go
+++ ds2bq/main.go
@@ -22,16 +22,15 @@ type SchedulerJob struct {
 }
 
 type Config struct {
-	ProjectID         string   `json:"projectID" yaml:"projectID"`
-	OutputGCSFilePath string   `json:"outputGCSFilePath" yaml:"outputGCSFilePath"`
+	ProjectID         string   `json:"projectId" yaml:"projectId"`
 	AllKinds          bool     `json:"allKinds" yaml:"allKinds"`
-	BQLoadProjectID   string   `json:"bqLoadProjectID" yaml:"bqLoadProjectID"`
-	BQLoadDatasetID   string   `json:"bqLoadDatasetID" yaml:"bqLoadDatasetID"`
 	Kinds             []string `json:"kinds"`
-	NamespaceIDs      []string `json:"namespaceIds"`
-	IgnoreKinds       []string `json:"ignoreKinds"`
-	IgnoreBQLoadKinds []string `json:"ignoreBQLoadKinds"`
-	MaxRetryCount     int      `json:"maxRetryCount"`
+	NamespaceIDs      []string `json:"namespaceIds" yaml:"namespaceIds"`
+	IgnoreKinds       []string `json:"ignoreKinds" yaml:"ignoreKinds"`
+	IgnoreBQLoadKinds []string `json:"ignoreBQLoadKinds" yaml:"ignoreBQLoadKinds"`
+	OutputGCSFilePath string   `json:"outputGCSFilePath" yaml:"outputGCSFilePath"`
+	BQLoadProjectID   string   `json:"bqLoadProjectId" yaml:"bqLoadProjectId"`
+	BQLoadDatasetID   string   `json:"bqLoadDatasetId" yaml:"bqLoadDatasetId"`
 }
 
 func ParseYaml(ctx context.Context, data []byte) ([]*SchedulerJob, error) {
